36ccaf260d946a7e2f6c9e584ed333980ec3969e
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-03-17 21:51:14 -0400

    working on section 3

---
+++ part-3/App.js
@@ -0,0 +1,18 @@
+function App() {
+    return (
+        <div>
+            <Person 
+                name= "Obie"
+                age= {16}
+            />,
+            <Person 
+                name= "Shelifinneous"
+                age= {36}
+            />,
+            <Person 
+                name= "Elijah"
+                age= {30}
+            />
+        </div>
+    )
+}
